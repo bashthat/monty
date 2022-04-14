d733f76921b5709a6abe852636e946a7e1e7002c
bashthat <3249@holbertonschool.com>
2022-04-14 12:28:37 -0700

    montymonty

---
+++ montymonty/push.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * push - push a element to a stack
+ * @stack: head of stack
+ * @line_number: line number
+ */
+
+void push(stack_t **stack, unsigned int line_number)
+{
+
+}
