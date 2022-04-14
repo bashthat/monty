fe5f552564bb45a82a7b7304b0af93cc326e0493
CalicoWhoMan <mikeyshrier@gmail.com>
2022-04-13 21:21:08 -0700

    gcc v.2

--- op_push.c
+++ op_push.c
@@ -9,9 +9,7 @@
 void op_push(stack_t **stack, __attribute__((unused)) unsigned int line_number)
 {
 	stack_t *nnode;
-	int x;
 
-	x = atoi(same);
 	nnode = malloc(sizeof(stack_t));
 
 	if (nnode == NULL)
